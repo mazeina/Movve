5f8872b0e7f8fbe6bf5761d543ec83b6e0d21160
mazeina <>
2022-09-05 17:27:59 +0300

    init commit

---
+++ Movve/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movve
+//
+//  Created by Полина Дусин on 05.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
